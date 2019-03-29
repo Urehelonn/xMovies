caed0730bfd846a9adaca687411356d834483656
Liam <urehelonn@gmail.com>
2019-03-29 03:47:30 -0400

    Add project files.

---
+++ xMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(xMovies.Startup))]
+namespace xMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
