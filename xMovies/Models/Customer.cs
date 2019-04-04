26097d85d0846a06690addf8f6e44026fc7bc0d4
Liam <urehelonn@gmail.com>
2019-04-04 15:13:38 -0400

    Data Annotation added

--- xMovies/Models/Customer.cs
+++ xMovies/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace xMovies.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool EmailSubscribed { get; set; }
         public MembershipType MembershipType { get; set; }
