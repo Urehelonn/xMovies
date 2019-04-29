7f6b8c8009b509d027292b1cf27118e7e8e06154
Liam <urehelonn@gmail.com>
2019-04-28 21:03:52 -0400

    customer api rebuild

---
+++ xMovies/Controllers/API/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using xMovies.Models;
+
+namespace xMovies.Controllers.API
+{
+    public class CustomerController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //get: api/customer
+        //return all customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            IEnumerable<Customer> customers = _context.Customers.ToList();
+            return customers;
+        }
+
+        //get: api/customer/:Id
+        //return single customer with required Id
+        public Customer GetCustomer(int Id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c=>c.Id==Id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return customer;
+        }
+
+        //post: api/customer
+        //post new customer to database
+        [HttpPost]
+        public Customer AddCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        //put: api/customer
+        //update existing customer and save to database
+        [HttpPut]
+        public void UpdateCustomer(int Id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var CustomerInDb = _context.Customers.SingleOrDefault(c => c.Id == Id);
+            if (CustomerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+        }
+    }
+}
--- xMovies/Global.asax.cs
+++ xMovies/Global.asax.cs
@@ -12,6 +12,7 @@ namespace xMovies
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
