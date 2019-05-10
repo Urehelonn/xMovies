374ebf80f739f30dc71fb6215fae380086dd5f26
Liam <urehelonn@gmail.com>
2019-05-10 00:38:38 -0400

    https config

--- xMovies/App_Start/FilterConfig.cs
+++ xMovies/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace xMovies
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
