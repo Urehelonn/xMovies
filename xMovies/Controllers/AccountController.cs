695773d94dd1a6fa7b61fc9945b02b7206eb6b4f
Liam <urehelonn@gmail.com>
2019-05-14 18:39:09 -0400

    after login / register / logoff redirect changed
    not to the home controller anymore, go to movie controller index instead

--- xMovies/Controllers/AccountController.cs
+++ xMovies/Controllers/AccountController.cs
@@ -170,7 +170,7 @@ namespace xMovies.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Movie");
                 }
                 AddErrors(result);
             }
@@ -399,7 +399,7 @@ namespace xMovies.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Movie");
         }
 
         //
@@ -456,7 +456,7 @@ namespace xMovies.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Movie");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
